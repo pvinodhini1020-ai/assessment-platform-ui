99df21ed5b18cc353c1d82345b8203b530a73f18
KARTHIKEYAN <pvinodhini1020@gmail.com>
2026-07-07 14:46:43 +0530

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({ server: { entry: "server" } }),
+    tanstackStart({ server: { entry: "./src/server.ts" } }),
     react(),
     tailwindcss(),
     tsConfigPaths(),
